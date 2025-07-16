b7b7896b5a8fbde546fea9c1a50da38be9539f6e
Calin Prutean <calin.prutean@gmail.com>
2025-07-15 19:33:18 -0700

    Basic framework for temp and humidity unit

--- Temperature_and_humidity_SU/Temperature_and_humidity_SU.ino
+++ Temperature_and_humidity_SU/Temperature_and_humidity_SU.ino
@@ -1,9 +1,42 @@
+#include <DHT.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#define DHTTYPE DHT11
+#define DHTPIN 4
+DHT dht(DHTPIN, DHTTYPE);
+
+typedef struct struct_message{
+  char* message;
+  float value;
+  int urgencyLevel;
+} struct_message;
+
+//Board address here 
+uint8_t broadcastAddress[] = {};
+
+
+const char* moduleType = "temp_and_humid_SU";
+
 void setup() {
   // put your setup code here, to run once:
-
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
+
+float requestTemp() {
+  return dht.readTemperature();
+}
+
+float requestHumidity() {
+  return dht.readHumidity();
+}
+
+
+
+
+
+
+
