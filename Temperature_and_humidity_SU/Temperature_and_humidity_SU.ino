fb03cd2029e3f9c1aaba4dd494c52ca5e2cb3eec
Calin Prutean <calin.prutean@gmail.com>
2025-07-18 22:02:29 -0700

    basic espNOW framework

--- Temperature_and_humidity_SU/Temperature_and_humidity_SU.ino
+++ Temperature_and_humidity_SU/Temperature_and_humidity_SU.ino
@@ -17,9 +17,24 @@ uint8_t broadcastAddress[] = {};
 
 const char* moduleType = "temp_and_humid_SU";
 
+struct_message MSG;
+
+esp_now_peer_info_t peerInfo;
+
+void onDataSent(const u_int8_t *addr, esp_now_send_status_t status) {
+
+}
+
 void setup() {
   // put your setup code here, to run once:
   dht.begin();
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing espNOW");
+    return;
+  }
 }
 
 void loop() {
