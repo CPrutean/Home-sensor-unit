1f04e32566e0a59c574f649d8741f01b52127279
Calin Prutean <calin.prutean@gmail.com>
2025-07-19 16:58:01 -0700

    Basic EspNOW framework

--- LCD_EspNOW_Example/LCD_EspNOW_Example.ino
+++ LCD_EspNOW_Example/LCD_EspNOW_Example.ino
@@ -1,5 +1,10 @@
 #include <LCD_I2C.h>
 #include <esp_now.h>
+#include <string.h>
+
+#define bool char
+#define true 1
+#define false 0
 
 #define buttonPin 21
 
@@ -7,12 +12,17 @@
 LCD_I2C lcd(0x27, 16, 2);
 
 typedef struct struct_message{
-  char* message;
+  char message[32];
   float value;
-  int urgencyLevel;
+  u_int8_t urgencyLevel;
 } struct_message;
 
-const u_int8_t broadcastAddress[] = {};
+const u_int8_t broadcastAddress[] = {0x3C, 0x8A, 0x1F, 0xD5, 0x44, 0xF8};
+esp_now_peer_info_t peerInfo;
+
+struct_message msg;
+
+
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(4, 5);
@@ -22,20 +32,72 @@ void setup() {
   
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
-
-  // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    return;
+    Serial.println("Error initializing espNOW");
+    exit(0);
   }
-  
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
+
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = true;
+
+  if (esp_now_add_peer(&peerInfo)!= ESP_OK) {
+    Serial.println("Failed to add peer");
+    exit(0);
+  }
+  esp_now_register_send_cb(OnDataSent);
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
-
+void onButtonPress() {
+  strncpy(msg.message, "PULL TEMP", strlen(msg.message));
+  esp_err_t result = esp_send_now(broadcastAddress, (u_int8_t*)&msg, sizeof(msg));
+  if (result == ESP_OK) {
+    Serial.println("Message was sent");
+  } else {
+    Serial.println("Message failed to send exiting");
+  }
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(msg.message);
+  lcd.setCursor(strlen(msg.message), 0);
+  lcd.print(msg.value);
+  
+  lcd.setCursor(0, 1);
+  strncpy(msg.message, "PULL HUMID", strlen("PULL HUMID"));
+  esp_err_t result = esp_send_now(broadcastAddress, (u_int8_t*)&msg, sizeof(msg));
+  if (result == ESP_OK) {
+    Serial.println("Message was sent");
+  } else {
+    Serial.println("Message failed to send exiting");
+  }
+  lcd.setCursor(strlen(msg.message), 1);
+  lcd.print(msg.value);
 }
+
+
+
+int temp;
+int button;
+void loop() {
+  button = digitalRead(buttonPin);
+  // put your main code here, to run repeatedly:
+  if (button == HIGH && temp == LOW) {
+    onButtonPress();
+  }
+  temp = button;
+}
+
+
+
+void onDataSent(const u_int8_t *addr, esp_now_send_status_t status) {
+  Serial.println("Packet sent");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery success":"Delivery failed");
+}
+
+void onDataRecv(const u_int8_t * adr, const u_int8_t * data, int len) {
+  memcpy(&msg, incomingData, sizeof(msg));
+}
+
--- Temperature_and_humidity_SU/Temperature_and_humidity_SU.ino
+++ Temperature_and_humidity_SU/Temperature_and_humidity_SU.ino
@@ -1,39 +1,67 @@
 #include <DHT.h>
 #include <esp_now.h>
 #include <WiFi.h>
+#include <string.h>
+
 #define DHTTYPE DHT11
 #define DHTPIN 4
 DHT dht(DHTPIN, DHTTYPE);
 
 typedef struct struct_message{
-  char* message;
+  char message[32];
   float value;
-  int urgencyLevel;
+  u_int8_t urgencyLevel;
 } struct_message;
 
 //Board address here 
-uint8_t broadcastAddress[] = {};
-
-
-const char* moduleType = "temp_and_humid_SU";
-
-struct_message MSG;
-
+uint8_t broadcastAddress[] = {0x3C, 0x8A, 0x1F, 0xD3, 0xD6, 0xEC};
+const char* module = "temp_and_humid_SU";
+struct_message msg;
 esp_now_peer_info_t peerInfo;
 
-void onDataSent(const u_int8_t *addr, esp_now_send_status_t status) {
-
-}
-
 void setup() {
-  // put your setup code here, to run once:
   dht.begin();
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
 
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing espNOW");
-    return;
+    exit(0);
+  }
+
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = true;
+
+  if (esp_now_add_peer(&peerInfo)!= ESP_OK) {
+    Serial.println("Failed to add peer");
+    exit(0);
+  }
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+}
+
+void parseMsg() {
+  if (msg.message[0] == NULL && msg.value == NULL) {
+    Serial.println("Message passed was null");
+    exit(0);
+  }
+
+  if (strncmp(msg.message, "PULL TEMP", strlen("PULL TEMP")) == 0) {
+    strncpy(msg.message, "TEMP", strlen("TEMP"));
+    msg.value = requestTemp();
+  } else if (strncmp(msg.message, "PULL HUMID", strlen("PULL HUMID"))) {
+    strncpy(msg.message, "HUMID", strlen("HUMID"));
+    msg.value = requestHumidity();
+  } else if (strncmp(msg.message, "GET TYPE", strlen("GET TYPE"))) {
+    strncpy(msg.message, "TYPE ", strlen("TYPE "));
+    strncat(msg.message, moduleType, strlen(moduleType));
+  }
+  esp_err_t result = esp_now_send(broadcastAddress, (u_int8_t*) &msg, sizeof(msg));
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  } else {
+    Serial.println("Message failed to send");
   }
 }
 
@@ -47,6 +75,16 @@ float requestTemp() {
 
 float requestHumidity() {
   return dht.readHumidity();
+}
+
+void onDataSent(const u_int8_t *addr, esp_now_send_status_t status) {
+  Serial.println("Packet sent");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery success":"Delivery failed");
+}
+
+void onDataRecv(const u_int8_t * adr, const u_int8_t * data, int len) {
+  memcpy(&msg, incomingData, sizeof(msg));
+  parseMsg();
 }
 
 
