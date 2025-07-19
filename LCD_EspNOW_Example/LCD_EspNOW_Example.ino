abc41df7acb43463ce33c1a258bb38714f7f369d
Calin Prutean <calin.prutean@gmail.com>
2025-07-18 21:22:03 -0700

    Implementation diagram and LCD practice example

---
+++ LCD_EspNOW_Example/LCD_EspNOW_Example.ino
@@ -0,0 +1,16 @@
+#include <LCD_I2C.h>
+#define buttonPin 21
+
+
+LCD_I2C lcd(0x27, 16, 2);
+
+const u_int8_t broadcastAddress[] = {};
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
