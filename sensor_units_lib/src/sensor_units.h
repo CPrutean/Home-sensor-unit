2c4244a0bbca5634b7663522b703e827ae6b40b5
Calin Prutean <calin.prutean@gmail.com>
2025-07-21 13:21:38 -0700

    Header file and library setup

--- sensor_units_lib/src/sensor_units.h
+++ sensor_units_lib/src/sensor_units.h
@@ -1,8 +1,10 @@
 #ifndef __SENSOR
 #define __SENSOR
 #include <Arduino.h>
+#include <string.h>
+#include <DHT.h>
 
-
+#define MAX_MSG_LENGTH 32
 enum sensor_type {TEMP_AND_HUMID, GPS, TIME};
 typedef struct _sensor {
     enum sensor_type *modules;
@@ -10,11 +12,13 @@ typedef struct _sensor {
 } sensor;
 
 typedef struct def_message_struct {
-    char message[32];
+    char message[MAX_MSG_LENGTH];
     int urgency;
     float value;
 } def_message_struct;
 
-void return_available_commands(char** array);
+int return_available_commands(char** array, int len, enum sensor_type sensor);
+int handleRequest(enum sensor_type sensor, char* cmd_passed, def_message_struct *response);
+
 
 #endif
