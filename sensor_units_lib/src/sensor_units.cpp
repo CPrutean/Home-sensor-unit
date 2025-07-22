7fbae2cc28c13082dd8e1a3340c8824edff72dd3
Calin Prutean <calin.prutean@gmail.com>
2025-07-21 20:03:35 -0700

    Changed file to cpp file

--- sensor_units_lib/src/sensor_units.cpp
+++ sensor_units_lib/src/sensor_units.cpp
@@ -1,18 +1,21 @@
 #include "sensor_units.h"
 
-const char* temp_sensor_cmds[] = {"PULL TEMP", "PULL HUMID"};
-const char* gps_sensor_cmds[] = {"PULL LOCATION"};
-const char* time_sensor_cmds[] = {"PULL TIME"};
+const char* temp_sensor_cmds[2] = {"PULL TEMP", "PULL HUMID"};
+const char* gps_sensor_cmds[1] = {"PULL LOCATION"};
+const char* time_sensor_cmds[1] = {"PULL TIME"};
 //Pass in an array with unlimited rows and the columns being 16 long
 int return_available_commands(char** array, int len, enum sensor_type sensor) {
     if (len != 16) {
         return -1;
     }
-
+    int i;
+    int len;
     switch (sensor) {
         case(TEMP_AND_HUMID):
-            strncpy(*(array), temp_sensor_cmds[0], 16);
-            strncpy(*(array+1), temp_sensor_cmds[1], 16);
+            len = sizeof(temp_sensor_cmds)/sizeof(temp_sensor_cmds[0]);
+            for (i = 0; i < len; i++) {
+                strncpy(*(array+i), temp_sensor_cmds[i], 16);
+            }
             break;
         case(GPS):
             strncpy(*(array), gps_sensor_cmds[0], 16);
@@ -26,13 +29,16 @@ int return_available_commands(char** array, int len, enum sensor_type sensor) {
     return 0;
 }
 
-int handleRequest(enum sensor_type sensor, char* cmd_passed, def_message_struct *response) {
+int handleRequest(enum sensor_type module, char* cmd_passed, def_message_struct *response, sensor CU) {
     memset(response, 0, sizeof(response));
     return 0;
 }
 
-void handleTempRequests(char* cmd_passed, def_message_struct *response) {
-
+void handleTempRequests(char* cmd_passed, def_message_struct *response, DHT dht) {
+    if (strncmp(cmd_passed, temp_sensor_cmds[0], 16) == 0) {
+        strncpy(response->message, "TEMP", 32);
+        response->value = dht.readTemperature();
+    }
 }
 
 void handleGpsRequests(char* cmd_passed, def_message_struct *response) {
