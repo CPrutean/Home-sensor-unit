68219e91e70d09452b46b92fdac72676c79e63c8
Calin Prutean <calin.prutean@gmail.com>
2025-09-08 21:14:34 -0700

    Included some databse logic, more to be implemented

--- Raspberry pi msg handler/database.py
+++ Raspberry pi msg handler/database.py
@@ -7,30 +7,49 @@ import os
 load_dotenv()
 database_path = os.getenv("DATABASE_PATH")
 db_lock = threading.Lock()
+insert_values = ["sensor", "sensor_unit", "communication_unit", "most_recent_reading"]
+
 
 
 def read(target_obj):
+    if (target_obj not in insert_values):
+        print("Error: target_obj wasnt in insert_values")
+        return
+
     return
 
+#Commas will separate all values that are passed and are lists
+def write(target_obj, write_list, update = False):
+    if (target_obj not in insert_values):
+        print("Error: target_obj wasnt in insert_values")
+        return
 
-def write(target_obj):
-    return
+    global db_lock
+    db_lock.acquire()
+    with sqlite3.connect(database_path) as conn:
+        print("balls")
+
+    
+    db_lock.release()
+    conn.close()
+
 
 def init():
-    #All values which need a list will be seperated by commas
-    default_tables = ["""CREATE TABLE IF NOT EXISTS sensor (sensor_id INTEGER UNIQUE NOT NULL, responses TEXT, requests TEXT)""",
+    default_tables = ["""CREATE TABLE IF NOT EXISTS sensor (name TEXT, responses TEXT, requests TEXT)""",
                       """CREATE TABLE IF NOT EXISTS sensor_unit (id INTEGER UNIQUE NOT NULL, name TEXT, sensors TEXT)""",
                       """CREATE TABLE IF NOT EXISTS communication_unit (id INTEGER unique NOT NULL, name TEXT, sensor_units TEXT)""",
                       """CREATE TABLE IF NOT EXISTS most_recent_reading (sensor_name TEXT, reading TEXT, values TEXT, timestamp TEXT)"""]
+    global database_path
 
     try:
         with sqlite3.connect(database_path) as conn:
             for command in default_tables:
                 conn.execute(command)
+        conn.close()
     except sqlite3.Error:
         print("Error: Unable to init database.")
 
-def sanitize():
+
+def sanitize(input):
     return
 
-    return
