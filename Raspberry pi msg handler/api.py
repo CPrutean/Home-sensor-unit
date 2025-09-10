6286e4592da288b530fa21045964f9c18e7e3c32
Calin Prutean <calin.prutean@gmail.com>
2025-09-09 21:12:44 -0700

    refactored to go back to old json system with some changes to normalize structure of Sensor, Sensor unit, and Communication unit objects

--- Raspberry pi msg handler/api.py
+++ Raspberry pi msg handler/api.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, jsonify
+import os
+import json
 import threading
+import serial
+from dotenv import load_dotenv
 
-lock = None
+load_dotenv()
+json_lock = None
 ser_port = None
+new_flag = False
+json_path = os.getenv("JSON_PATH")
 
 def assign_lock_and_ser_port(lock_in, ser_port_in):
     global lock
@@ -14,53 +21,193 @@ def assign_lock_and_ser_port(lock_in, ser_port_in):
     lock = lock_in
     ser_port = ser_port_in
 
+
+
 def run_app():
     app.run(host='0.0.0.0', port=80)
 
+def set_new_flag():
+    global new_flag
+    new_flag = True
 
 app=Flask(__name__)
+
+
 @app.route('/')
 def return_all():
-    return
-
+    global json_path
+    global json_lock
+    json_lock.acquire()
+    try:
+        with open(json_path, "r") as f:
+            return json.load(f)
+    except Exception as e:
+        print(f"Error: {e}")
+        return{"ERROR":"FAILED TO READ JSON FILE"}
+    except json.JSONDecodeError as e:
+        print(f"Error: {e}")
+        return{"ERROR":"FAILED TO PARSE JSON FILE"}
+    finally:
+        json_lock.release()
 
 @app.route('/sensors', methods = ["GET"])
-def return_sensors(lock):
-    return
+def return_sensors():
+    global json_path
+    global json_lock
+    json_lock.acquire()
+    try:
+        with open(json_path, "r") as f:
+            data = json.load(f)
+        return jsonify(data["Sensors"])
+    except Exception as e:
+        print(f"Error: {e}")
+        return{"ERROR":"FAILED TO READ JSON FILE"}
+    except json.JSONDecodeError as e:
+        print(f"Error: {e}")
+        return{"ERROR":"FAILED TO PARSE JSON FILE"}
+    finally:
+        json_lock.release()
+
 
 @app.route('/sensor/<int:sensor_id>', methods = ["GET"])
-def return_sensor(sensor_ind):
-    return
+def return_sensor(sensor_id):
+    global json_path
+    global json_lock
+    json_lock.acquire()
+    try:
+        with open(json_path, "r") as f:
+            data = json.load(f)
+        for sensor in data["Sensors"]:
+            if sensor["ID"] == sensor_id:
+                return jsonify(sensor)
+        return{"ERROR":"NO SENSOR WITH THAT ID"}
+    except Exception as e:
+        return{"ERROR":"FAILED TO READ JSON FILE"}
+    except json.JSONDecodeError as e:
+        return{"ERROR":"FAILED TO PARSE JSON FILE"}
+    finally:
+        json_lock.release()
 
 #will return all readings for a sensor unit
 @app.route('/readings/', methods = ["GET"])
 def return_readings():
-    return
+    global json_path
+    global json_lock
+    json_lock.acquire()
+    try:
+        with open(json_path, "r") as f:
+            data = json.load(f)
+        return jsonify(data["Most recent readings"])
+    except Exception as e:
+        return{"ERROR":"FAILED TO READ JSON FILE"}
+    except json.JSONDecodeError as e:
+        return{"ERROR":"FAILED TO PARSE JSON FILE"}
+    finally:
+        json_lock.release()
 
-@app.route('/readings/<int:sensor_id>', methods = ["GET"])
-def return_readings_for_sensor(sensor_id):
-    return
+@app.route('/readings/<string:sensor_name>', methods = ["GET"])
+def return_readings_for_sensor(sensor_name):
+    global json_path
+    global json_lock
+    json_lock.acquire()
+    try:
+        with open(json_path, "r") as f:
+            data = json.load(f)
+        reading_list = []
+        found = False
+        for reading in data["Most recent readings"]:
+            if reading["Sensor"] == sensor_name:
+                found = True
+                reading_list.append(reading)
+        if found:
+            return jsonify({sensor_name:reading_list})
+        else:
+            return{"ERROR":"NO READINGS FOR THAT SENSOR"}
+    except Exception as e:
+        return{"ERROR":"FAILED TO READ JSON FILE"}
+    except json.JSONDecodeError as e:
+        return{"ERROR":"FAILED TO PARSE JSON FILE"}
+    finally:
+        json_lock.release()
 
-@app.route('/readings/<string:response>', methods = ["GET"])
-def return_readings_for_response(response):
-    return
+@app.route('/readings/<string:Reading>', methods = ["GET"])
+def return_readings_for_response(Reading):
+    global json_path
+    global json_lock
+    json_lock.acquire()
+    try:
+        with open(json_path, "r") as f:
+            data = json.load(f)
+        for reading in data["Most recent readings"]:
+            if reading["Reading"] == Reading:
+                return jsonify({Reading:reading})
+        return{"ERROR":"NO READINGS FOR THAT RESPONSE"}
+    except Exception as e:
+        return{"ERROR":"FAILED TO READ JSON FILE"}
+    except json.JSONDecodeError as e:
+        return{"ERROR":"FAILED TO PARSE JSON FILE"}
+    finally:
+        json_lock.release()
 
 
 @app.route('/sensor_units/', methods = ["GET"])
 def return_sensor_units():
-    return
+    global json_path
+    global json_lock
+    json_lock.acquire()
+    try:
+        with open(json_path, "r") as f:
+            data = json.load(f)
+        return jsonify({"Sensor units": data["Sensor units"]})
+    except Exception as e:
+        return{"ERROR":"FAILED TO READ JSON FILE"}
+    except json.JSONDecodeError as e:
+        return{"ERROR":"FAILED TO PARSE JSON FILE"}
+    finally:
+        json_lock.release()
+
 
 @app.route('/sensor_units/<int:sensor_unit_id>', methods = ["GET"])
 def return_sensor_unit(sensor_unit_id):
-    return
+    global json_path
+    global json_lock
+    json_lock.acquire()
+    try:
+        with open(json_path, "r") as f:
+            data = json.load(f)
+        for sensor_unit in data["Sensor units"]:
+            if sensor_unit["ID"] == sensor_unit_id:
+                return jsonify(sensor_unit)
+        return{"ERROR":"NO SENSOR WITH THAT ID"}
+    except Exception as e:
+        return{"ERROR":"FAILED TO READ JSON FILE"}
+    except json.JSONDecodeError:
+        return{"ERROR":"FAILED TO PARSE JSON FILE"}
+    finally:
+        json_lock.release()
 
 @app.route('/communication_units/', methods = ["GET"])
 def return_communication_units():
-    return
+    global json_path
+    global json_lock
+    json_lock.acquire()
+    try:
+        with open(json_path, "r") as f:
+            data = json.load(f)
+        return jsonify({"Communication units": data["Communication units"]})
+    except Exception as e:
+        return{"ERROR":"FAILED TO READ JSON FILE"}
+    except json.JSONDecodeError as e:
+        return{"ERROR":"FAILED TO PARSE JSON FILE"}
+    finally:
+        json_lock.release()
 
-@app.route('/sensor_units/<int:sens_unit_id>/send_command/<string:command>', methods = ['POST'])
-def send_push(command, sens_unit_id):
-    return
+@app.route('communication_units/<string:send_command>', methods = ['POST'])
+def send_push(send_command):
+    global ser_port
+    temp = send_command + "\n"
+    ser_port.write(send_command.encode('utf-8'))
+    return "OK"
 
 #Only works when called directly for testing purposes
 if __name__ == '__main__':
