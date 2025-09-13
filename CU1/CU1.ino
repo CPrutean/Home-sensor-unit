cac188ae273a45f6c452b4638878aef26bfbdf51
Calin Prutean <calin.prutean@gmail.com>
2025-09-13 00:16:30 -0700

    Updated controller unit code

--- CU1/CU1.ino
+++ CU1/CU1.ino
@@ -114,7 +114,6 @@ void setup() {
   xTaskCreatePinnedToCore(retryTask, "retryFailedMessages", 8192, NULL, 3, NULL, 1);
   xTaskCreatePinnedToCore(queueHandlerTask, "Queue Handler Task", 8192, NULL, 2, NULL, 1);
   xTaskCreatePinnedToCore(serialHandlerTask, "Serial Handler Task", 8192, NULL, 1, NULL, 1);
-  respondPiRequest("INIT|PI|");
 }
 
 void loop() {
