97860f8dab65e1f072dcbfd3228012810fde84d2
CPrutean <calin.prutean@gmail.com>
2025-12-21 20:19:06 -0800

    Changed global header files to include optional

--- Sensor-units-firmware/src/MessageAck.h
+++ Sensor-units-firmware/src/MessageAck.h
@@ -5,8 +5,8 @@ class MessageAck final {
 public:
   MessageAck();
   // Returns the msgID of a finished packet list if one exists
-  unsigned long long insert(unsigned long long msgID, const Packet &p);
-  ackListItem remove(unsigned long long msgID);
+  std::optional<ackListItem> insert(unsigned long long msgID, const Packet &p);
+  void remove(unsigned long long msgID);
   ~MessageAck();
 
 private:
