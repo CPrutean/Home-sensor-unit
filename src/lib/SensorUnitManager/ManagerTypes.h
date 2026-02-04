87e22cab5d0dd6cebcd99900d3edecd3b021f628
Calin Prutean <calinprutean@Calins-MacBook-Pro.local>
2026-02-04 00:07:54 -0800

    Reworked sensor readings for faster performance with altered memory structure

--- src/lib/SensorUnitManager/ManagerTypes.h
+++ src/lib/SensorUnitManager/ManagerTypes.h
@@ -14,19 +14,16 @@ enum class SensorUnitStatus : uint8_t {ONLINE = 0, ERROR, OFFLINE, NUMTYPES};
 class SensorUnitReadings final {
 public:
   explicit SensorUnitReadings();
-  //Information will be stored as raw bytes until we have need it
-  void postReading(const uint8_t *data, uint8_t readingSize, PacketInfo_t readingInfo);
+  void postReading(const Packet &p);
   int getReadingCount();
-  void setReadingCount(int readingCount); //Will also resize the internal arrays to accomodate for size if need be
-  void getReading(uint8_t *buffer, uint8_t bufferSize, PacketInfo_t packet);
+  void postNewSensor(Sensors_t sensor, uint8_t readingCount);
+  Packet& getReading(PacketInfo_t packet);
 private:
-  //All should be treated as arrays
-  uint8_t readingCount{0}; //Treated as the length
-  uint8_t **readings{nullptr};
-  uint8_t *readingSizeArray{nullptr};  //Treated as the length of the sub arrays of the readings
-  PacketInfo_t *readingInfo{nullptr}; //Info of the sensor and the reading index
-  uint8_t readingCapacity{0};
-  xSemaphoreHandle mutex;
+  Packet** packetGroupings{nullptr};
+  uint8_t* packetCount{nullptr};
+  uint8_t sensorCount{0};
+  
+  xSemaphoreHandle mutex = xSemaphoreCreateRecursiveMutex();
 };
 
 
@@ -37,6 +34,7 @@ struct SensorUnitInfo {
   esp_now_peer_info_t peerInf{};
   uint8_t sensorCount{0};
   SensorUnitStatus status{};
+  unsigned long SensorUnitID{0};
 };
 
 
