4767a7b612087438b38937e20c99bd5874e4a6bd
Calin Prutean <calinprutean@Calins-MacBook-Pro.local>
2026-02-04 09:29:42 -0800

    Reworked MessageAck to store total requests instead of working on an ID based system

--- src/lib/Components/MessageAck.cpp
+++ src/lib/Components/MessageAck.cpp
@@ -2,114 +2,92 @@
 #include <SensorUnitManager/SensorUnitManager.h>
 #define MAXTIMEOUT 10000
 
-/** 
-@breif: Default constructor for MessageAck object
-*/
-MessageAck::MessageAck() {
-  ackArr = new ackListItem[BEGINACKLEN];
-  arrSize = BEGINACKLEN;
-  mutex = xSemaphoreCreateRecursiveMutex();
-}
 
-/** 
-@breif: Resizes internal array for improved performance when needed
-*/
-void MessageAck::resize() {
-  if (xSemaphoreTakeRecursive(mutex, portMAX_DELAY) != pdTRUE) {
-    Serial.println("Failed to take MUTEX");
-    return;
-  }
-
-
-  if (ackArr == nullptr) {
-    Serial.println("Resize failed, object was never initialized");
-    xSemaphoreGiveRecursive(mutex);
-    return;
-  }
-
-  size_t newCapacity = capacity * 2;
-  ackListItem *newArr = new ackListItem[newCapacity];
-  memcpy(newArr, ackArr, sizeof(ackListItem) * size);
-  delete[] ackArr;
-  capacity = newCapacity;
-  ackArr = newArr;
-  xSemaphoreGiveRecursive(mutex);
-}
-
-/** 
-@breif: removes an ackArrItem based on the group ID passed
-@param: unsigned long long msgID: message group ID to be removed
-*/
-bool MessageAck::removeAckArrItem(unsigned long long msgID) {
-  if (xSemaphoreTakeRecursive(mutex, portMAX_DELAY) != pdTRUE) {
-    Serial.println("Failed to take mutex");
-    return false;
-  }
-
-
-  int i;
-  bool found = false;
-  for (i = 0; i < size; i++) {
-    if (msgID == ackArr[i].msgID) {
-      found = true;
-      break;
+uint8_t MessageAck::getSuArrInd(unsigned long id) {
+    if (xSemaphoreTakeRecursive(mutex, portMAX_DELAY) != pdTRUE) {
+        Serial.println("Failed to take mutex");
+        return;
+    } 
+    bool found = false;
+    int i;
+    for (i = 0; i < suCount; i++) {
+        if (id == idArray[i]) {
+            found = 1;
+            break;
+        }
     }
-  }
 
-  if (!found) {
-    Serial.println("Invalid msgID");
+    uint8_t temp = found ? i : 255;
     xSemaphoreGiveRecursive(mutex);
-    return false;
-  }
-
-  for (int j{i}; j < static_cast<int>(size)-1; j++) {
-    ackArr[j] = ackArr[j+1];
-  }
-  size--;
-  xSemaphoreGiveRecursive(mutex);
-  return true;
-}
-/** 
-@breif: creates a new ackListItem object with a new msg groupID
-@param: message group ID to associate with different packets
-*/
-void MessageAck::addNewAckArrItem(unsigned long long msgID, unsigned long long postTime) {
-  if (xSemaphoreTakeRecursive(mutex, portMAX_DELAY) != pdTRUE) {
-    Serial.println("Failed to take mutex");
-    return;
-  }
-
-
-  if (size >= capacity) {
-    resize();
-  }
-  ackArr[size] = ackListItem{msgID, millis()};
-  size++;
-
-  xSemaphoreGiveRecursive(mutex);
-}
-/**
- * @breif: will remove timed out requests from the message ack queue and increment the error status of a request
- * @param: suNumList, an array of Sensor unit status objects. The index of the array corresponds to the sensor units inside the Sensor unit managers
- */
-void MessageAck::removeTimedOutReq(SensorUnitStatus* suNumList) {
-  if (xSemaphoreTakeRecursive(mutex, portMAX_DELAY) != pdTRUE) {
-    Serial.println("Failed to take mutex");
-    return;
-  }
-  const unsigned long now = millis();
-  for (int i{0}; i < capacity; i++) {
-    unsigned long time = now-ackArr[i].postTime;
-    if (time > MAXTIMEOUT && suNumList[ackArr[i].suInd] != SensorUnitStatus::OFFLINE) {
-      suNumList[ackArr[i].suInd] = static_cast<SensorUnitStatus>(static_cast<uint8_t>(suNumList[ackArr[i].suInd])+1);
-    } //Assume that the device is just dropping packets at first, if more than 1 packet is faulty assume offline state
-  }
-  xSemaphoreGiveRecursive(mutex);
+    return temp;
 }
 
+void MessageAck::addSensorUnit(unsigned long suID) {
+    if (xSemaphoreTakeRecursive(mutex, portMAX_DELAY) != pdTRUE) {
+        Serial.println("Failed to take mutex");
+        return;
+    } else {
+        idArray[suCount++] = suID;
+    }
+
+    xSemaphoreGiveRecursive(mutex);
+}
+
+
+void MessageAck::expectPacket(unsigned long suID) {
+    if (xSemaphoreTakeRecursive(mutex, portMAX_DELAY) != pdTRUE) {
+        Serial.println("Failed to take mutex");
+        return;
+    }
+    uint8_t ind;
+
+    if ((ind = getSuArrInd(suID)) != 255) {
+        packetsRequested[ind]++;
+    } else {
+        Serial.println("Invalid su array index");
+    }
+   xSemaphoreGiveRecursive(mutex);
+    
+}
+
+
+void MessageAck::packetRecived(unsigned long suID) {
+    if (xSemaphoreTakeRecursive(mutex, portMAX_DELAY) != pdTRUE) {
+        Serial.println("Failed to take mutex");
+        return;
+    } 
+    
+    uint8_t ind;
+    if ((ind = getSuArrInd(suID)) != 255) {
+        packetsRecieved[ind]++;
+    } else {
+        Serial.println("Invalid su array index");
+    }
+
+    xSemaphoreGiveRecursive(mutex);
+}
+
+
+double MessageAck::getPacketDropPercentage(unsigned long suID) {
+    if (xSemaphoreTakeRecursive(mutex, portMAX_DELAY) != pdTRUE) {
+        Serial.println("Failed to take mutex");
+        return;
+    } 
+
+    uint8_t ind;
+    double temp;
+    if ((ind = getSuArrInd(suID)) != 255) {
+        temp = static_cast<double>(packetsRecieved[ind])/static_cast<double>(packetsRequested[ind]);
+    } else {
+        Serial.println("Invalid su array index");
+        temp = -1;
+    }
+    xSemaphoreGiveRecursive(mutex);
+    return temp;
+}
+  
 /** 
 @breif: destructor frees internal ackListItem array
 */
 MessageAck::~MessageAck() {
-  delete[] ackArr;
 }
