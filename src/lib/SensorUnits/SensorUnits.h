81e03b9396895bc590a82d07eeffec74df48b75f
Calin Prutean <calinprutean@Calins-MacBook-Pro.local>
2026-02-09 12:54:14 -0800

    Small line changes

--- src/lib/SensorUnits/SensorUnits.h
+++ src/lib/SensorUnits/SensorUnits.h
@@ -3,8 +3,9 @@
 #include <Core/global_include.h>
 #include <DHT.h>
 #include <PIR.h>
-#include <esp_now.h>
 #include <Wire.h>
+#include <esp_now.h>
+
 // As more sensors get added certain things will be added onto but the
 // SensorUnitManager should behave the exact same
 #define MAXSENSORS 3
@@ -14,19 +15,23 @@ public:
   void handlePacket(const Packet &packet);
   MessageQueue msgQueue{};
   void initESPNOW();
-  explicit SensorUnit(const uint8_t *cuMac, const char *PMKKEYIN, const char *LMKKEYIN, DHT *tempIn = nullptr, PIR *motion = nullptr);
-  void sendPacket(const Packet& p);
+  explicit SensorUnit(const uint8_t *cuMac, const char *PMKKEYIN,
+                      const char *LMKKEYIN, DHT *tempIn = nullptr,
+                      PIR *motion = nullptr);
+  void sendPacket(const Packet &p);
 
-  #ifndef TESTING
+#ifndef TESTING
   SensorUnit() = delete;
-  #else
+#else
   explicit SensorUnit();
-  #endif
-  friend void sendAllPackets(SensorUnit& sensUnit); //Outside of these 2 friend methods, no sensors should have access to internal private members
-  friend void baseCommands(SensorUnit& sensUnit, Packet& p, uint8_t ind);
+#endif
+  friend void sendAllPackets(
+      SensorUnit &sensUnit); // Outside of these 2 friend methods, no sensors
+                             // should have access to internal private members
+  friend void baseCommands(SensorUnit &sensUnit, Packet &p, uint8_t ind);
   DHT *temp{nullptr};
   PIR *motion{nullptr};
-  void initSensorDefinition(SensorDefinition& SensorDefinition);
+
 private:
   esp_now_peer_info_t cuPeerInf{};
   char PMKKEY[16]{};
@@ -34,3 +39,11 @@ private:
   SensorDefinition sensorsAvlbl[MAXSENSORS]{};
   uint8_t sensCount{};
 };
+
+using defaultFnMethods = void (*)(SensorUnit &sensorPtr, Packet &p, uint8_t);
+
+void writeErrorMsg(Packet &p, const char *errormsg);
+void initSensorDefinition(SensorDefinition &SensorDefinition);
+void tempCommands(SensorUnit &sensUnit, Packet &p, uint8_t ind);
+void motionCommands(SensorUnit &sensUnit, Packet &p, uint8_t ind);
+void baseCommands(SensorUnit &, Packet &p, uint8_t ind);
