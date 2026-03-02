0f7e170d05455f6285b60ae4a1bc10c9c505eae8
Calin Prutean <calinprutean@Calins-MacBook-Pro.local>
2026-03-01 21:47:15 -0800

    Updated sketchy to include new logic

--- src/SensorUnit1/main.cpp
+++ src/SensorUnit1/main.cpp
@@ -1,31 +1,32 @@
 #include <Arduino.h>
 #include <PIR.h>
 #include <SensorUnits/SensorUnits.h>
+
 #include "../Local_config.h"
-#include <WebServer-SUM-portal/Portal.h>
 
 #define PIRPIN 13
 PIR pir;
 SensorUnit SU1(CONFIG::CUMAC, CONFIG::PMKKEY, CONFIG::SUMLMKKEY, nullptr, &pir);
 
 void packetHandlerTask(void* params) {
-    Packet p;
-    for (;;) {
-        if (SU1.msgQueue.receive(p)) {
-            SU1.handlePacket(p);
-        }
-        vTaskDelay(10/portTICK_PERIOD_MS);
+  Packet p;
+  for (;;) {
+    if (SU1.msgQueue.receive(p)) {
+      SU1.handlePacket(p);
     }
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
 }
 
 void setup() {
-    Serial.begin(115200);
-    pir.add(PIRPIN);
-    pinMode(PIRPIN, INPUT);
-    SU1.initESPNOW();
-    xTaskCreatePinnedToCore(packetHandlerTask, "Packet Handler Task", 8192, NULL, 1, NULL, 1);
+  Serial.begin(115200);
+  SU1.initESPNOW();
+
+  pir.add(PIRPIN);
+  pinMode(PIRPIN, INPUT);
+
+  xTaskCreatePinnedToCore(packetHandlerTask, "Packet Handler Task", 8192, NULL,
+                          1, NULL, 1);
 }
 
-void loop() {
-
-}
+void loop() {}
